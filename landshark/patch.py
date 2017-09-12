231308946256b10fb4ad6bb874a022cb46fae975
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2017-09-12 16:14:35 +1000

    Simplify. Alot.

--- landshark/patch.py
+++ landshark/patch.py
@@ -7,57 +7,6 @@ import numpy as np
 log = logging.getLogger(__name__)
 
 
-class PatchRange:
-    """
-    Fixed-y xrange for contiguous patch strips.
-
-    Parameters
-    ----------
-    y : int
-        The y index of the strip
-    xstart : int
-        The start of the x range of the strip
-    xend : int
-        The exclusive end indeex, so array[xstart:xend] is correct.
-
-    """
-
-    def __init__(self, y: int, xstart: int, xstop: int) -> None:
-        """Create a PatchRange object."""
-        self.y = y
-        self.x = slice(xstart, xstop)
-
-
-class PatchRead:
-    """
-    An abstraction of the patch reading operation.
-
-    Contains two slices of the same size, one to fill the patch,
-    and one to index the image.
-
-    Parameters
-    ----------
-    y : int
-        The y coordinate of the read
-    xstart : int
-        The start of the x coordinate of the read
-    xstop : int
-        The exclusive end x coordinate of the read
-    x_offset : int
-        The x offset of the patch in pixels (in the image).
-    y_offset : int
-        The y offset of the patch in pixels (in the image).
-
-    """
-
-    def __init__(self, y: int, xstart: int, xstop: int, x_offset: int,
-                 y_offset: int) -> None:
-        """Create a PatchRead object."""
-        self.image = PatchRange(y, xstart, xstop)
-        self.patch = PatchRange(y - y_offset, xstart - x_offset,
-                                xstop - x_offset)
-
-
 class Patch:
     """
     Contains all the information required to read a patch.
@@ -89,19 +38,18 @@ class Patch:
         n = halfwidth * 2 + 1
         ymax = ymin + n - 1  # INCLUSIVE
         xmax = xmin + n - 1  # INCLUSIVE
-        mask = np.ones((n, n), dtype=bool)
+        self.mask = np.ones((n, n), dtype=bool)
         # What lines to read?
         ystart = max(0, ymin)
         xstart = max(0, xmin)
         ystop = min(image_height, ymax + 1)  # EXCLUSIVE
         xstop = min(image_width, xmax + 1)  # EXCLUSIVE
 
-        y_lines = range(ystart, ystop)
-        reads = [PatchRead(y, xstart, xstop, xmin, ymin) for y in y_lines]
+        self.y_indices = np.arange(ystart, ystop)
 
-        # create the mask
-        for r in reads:
-            mask[r.patch.y, r.patch.x] = False
+        self.patch_y_indices = self.y_indices - ymin
+        self.x = slice(xstart, xstop)
+        self.patch_x = slice(xstart - xmin, xstop - xmin)
 
-        self.reads = reads
-        self.mask = mask
+        for y in self.patch_y_indices:
+            self.mask[y, self.patch_x] = False
--- tests/test_patch.py
+++ tests/test_patch.py
@@ -19,45 +19,55 @@ def test_patches():
     x = 0
     y = 0
     p = patch.Patch(x, y, halfwidth, im_width, im_height)
-    for r in p.reads:
-        p_data[r.patch.y, r.patch.x] = image[r.image.y, r.image.x]
+    for yi, patch_yi in zip(p.y_indices, p.patch_y_indices):
+        p_data[patch_yi, p.patch_x] = image[yi, p.x]
     true_answer = np.array([[-1, -1, -1],
                             [-1, 0, 1],
                             [-1, 5, 6]], dtype=int)
+    true_mask = true_answer == -1
     assert np.all(true_answer == p_data)
+    assert np.all(true_mask == p.mask)
 
     # 4,4 corner
     p_data = np.zeros((n, n), dtype=int) - 1
     x = 4
     y = 4
     p = patch.Patch(x, y, halfwidth, im_width, im_height)
-    for r in p.reads:
-        p_data[r.patch.y, r.patch.x] = image[r.image.y, r.image.x]
+    for yi, patch_yi in zip(p.y_indices, p.patch_y_indices):
+        p_data[patch_yi, p.patch_x] = image[yi, p.x]
     true_answer = np.array([[18, 19, -1],
                             [23, 24, -1],
                             [-1, -1, -1]], dtype=int)
+    true_mask = true_answer == -1
     assert np.all(true_answer == p_data)
+    assert np.all(true_mask == p.mask)
 
     # 0,2 edge
     p_data = np.zeros((n, n), dtype=int) - 1
     x = 0
     y = 2
     p = patch.Patch(x, y, halfwidth, im_width, im_height)
-    for r in p.reads:
-        p_data[r.patch.y, r.patch.x] = image[r.image.y, r.image.x]
+    for yi, patch_yi in zip(p.y_indices, p.patch_y_indices):
+        p_data[patch_yi, p.patch_x] = image[yi, p.x]
     true_answer = np.array([[-1, 5, 6],
                             [-1, 10, 11],
                             [-1, 15, 16]], dtype=int)
+    true_mask = true_answer == -1
     assert np.all(true_answer == p_data)
+    assert np.all(true_mask == p.mask)
+
 
     # 2,0 edge
     p_data = np.zeros((n, n), dtype=int) - 1
     x = 2
     y = 0
     p = patch.Patch(x, y, halfwidth, im_width, im_height)
-    for r in p.reads:
-        p_data[r.patch.y, r.patch.x] = image[r.image.y, r.image.x]
+    for yi, patch_yi in zip(p.y_indices, p.patch_y_indices):
+        p_data[patch_yi, p.patch_x] = image[yi, p.x]
     true_answer = np.array([[-1, -1, -1],
                             [1, 2, 3],
                             [6, 7, 8]], dtype=int)
+    true_mask = true_answer == -1
     assert np.all(true_answer == p_data)
+    assert np.all(true_mask == p.mask)
+
