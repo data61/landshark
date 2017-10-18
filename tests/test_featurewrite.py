3304e1113d1ea03422ab8441092455b4e9e16aaf
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2017-10-19 10:12:52 +1100

    Add masked test for stats

--- tests/test_featurewrite.py
+++ tests/test_featurewrite.py
@@ -122,6 +122,28 @@ def test_statistics():
     assert np.allclose(var, true_var)
 
 
+def test_statistics_masked():
+    n_features = 2
+    n_rows = 10
+    n_blocks = 5
+    stats = featurewrite._Statistics(n_features)
+    data = [np.random.randn(n_rows, n_features) for i in range(n_blocks)]
+    masks = [np.random.choice(2, size=(n_rows, n_features)).astype(bool)
+             for i in range(n_blocks)]
+    all_data = np.concatenate(data, axis=0)
+    all_masks = np.concatenate(masks, axis=0)
+    all_marray = np.ma.MaskedArray(data=all_data, mask=all_masks)
+    m_data = [np.ma.MaskedArray(data=d, mask=m) for d, m in zip(data, masks)]
+    for d in m_data:
+        stats.update(d)
+    mean = stats.mean
+    var = stats.variance
+    true_mean = np.ma.mean(all_marray, axis=0)
+    true_var = np.var(all_marray, axis=0)
+    assert np.allclose(mean, true_mean)
+    assert np.allclose(var, true_var)
+
+
 def test_to_masked():
     cols = 4
     rows = 10
