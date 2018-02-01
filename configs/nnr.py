541319739b4c1c636e3955b95e905e0205d78fa4
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2018-02-01 17:29:13 +1100

    simple generic nn configs for reg and class

---
+++ configs/nnr.py
@@ -0,0 +1,43 @@
+"""Generic regression config file."""
+import numpy as np
+import tensorflow as tf
+import aboleth as ab
+
+from landshark.model import patch_slices
+
+ab.set_hyperseed(666)
+noise0 = 0.1
+embed_dim = 3
+
+
+def model(Xo, Xom, Xc, Xcm, Y, samples, metadata):
+    noise = tf.Variable(noise0 * np.ones(metadata.ntargets, dtype=np.float32))
+    slices = patch_slices(metadata)
+
+    # Categorical features
+    embed_layers = [ab.EmbedMAP(embed_dim, k, l1_reg=1e-5, l2_reg=0.)
+                    for k in metadata.ncategories]
+
+    cat_net = (
+        ab.InputLayer(name="Xc", n_samples=samples) >>
+        ab.PerFeature(*embed_layers, slices=slices)
+        )
+
+    # Continuous features
+    data_input = ab.InputLayer(name="Xo", n_samples=samples)  # Data input
+    mask_input = ab.MaskInputLayer(name="Mo")  # Missing data mask input
+
+    con_net = ab.LearnedScalarImpute(data_input, mask_input)
+
+    # Combined net
+    net = (
+        ab.Concat(con_net, cat_net) >>
+        ab.Activation(tf.nn.elu) >>
+        ab.DenseMAP(output_dim=1, l1_reg=0., l2_reg=1e-5)
+        )
+
+    F, reg = net(Xo=Xo, Mo=Xom, Xc=Xc)
+    lkhood = tf.distributions.StudentT(df=5., loc=F, scale=ab.pos(noise))
+    loss = ab.max_posterior(lkhood, Y, reg)
+
+    return F, lkhood, loss, Y
