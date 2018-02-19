fix for complete missing columns in batches when normalising

landshark/normalise.py
@@ -19,11 +19,14 @@ class Normaliser:
         assert array.shape[0] > 1
 
         new_n = np.ma.count(array, axis=0)
-        new_mean = np.ma.mean(array, axis=0)
-        new_m2 = np.ma.var(array, axis=0, ddof=0) * new_n
+        new_mean = (np.ma.mean(array, axis=0)).data
+        new_m2 = (np.ma.var(array, axis=0, ddof=0) * new_n).data
 
         delta = new_mean - self._mean
-        delta_mean = delta * (new_n / (new_n + self._n))
+        add_n = new_n + self._n
+        if any(add_n == 0):  # catch any totally masked images
+            add_n[add_n == 0] = 1
+        delta_mean = delta * (new_n / add_n)
 
         self._mean += delta_mean
         self._m2 += new_m2 + (delta * self._n * delta_mean)