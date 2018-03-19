df4304fb1e720b7cef3f96997efcaa63963e8c3d
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2018-03-19 15:42:08 +1100

    hread typechecks

--- landshark/hread.py
+++ landshark/hread.py
@@ -1,8 +1,9 @@
 """Read features and targets from HDF5 files."""
 
+from types import TracebackType
 import numpy as np
 import tables
-
+from typing import Union, Tuple
 from landshark.image import ImageSpec
 from landshark.basetypes import ArraySource, OrdinalArraySource, \
     CategoricalArraySource, CoordinateArraySource, FeatureValues, FixedSlice
@@ -13,7 +14,7 @@ class H5ArraySource(ArraySource):
 
     _array_name = ""
 
-    def __init__(self, path) -> None:
+    def __init__(self, path: str) -> None:
         self._path = path
         with tables.open_file(self._path, "r") as hfile:
             carray = hfile.get_node("/" + self._array_name)
@@ -24,22 +25,24 @@ class H5ArraySource(ArraySource):
             self._native = carray.chunkshape[0]
             self._dtype = carray.atom.dtype.base
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         self._hfile = tables.open_file(self._path, "r")
         self._carray = self._hfile.get_node("/" + self._array_name)
         if hasattr(self._hfile.root, "coordinates"):
             self._coords = self._hfile.root.coordinates
         super().__enter__()
 
-    def __exit__(self, ex_type, ex_value, ex_traceback):
+    def __exit__(self, ex_type: type, ex_val: Exception,
+                 ex_tb: TracebackType) -> None:
         self._hfile.close()
         del(self._carray)
         if hasattr(self, "_coords"):
             del(self._coords)
         del(self._hfile)
-        super().__exit__(ex_type, ex_value, ex_traceback)
+        super().__exit__(ex_type, ex_val, ex_tb)
 
-    def _arrayslice(self, start: int, end: int) -> np.ndarray:
+    def _arrayslice(self, start: int, end: int) -> \
+            Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
         """
         TODO: Note this is bad because I'm changing the return type.
         """
@@ -63,7 +66,7 @@ class H5Features:
     """
     Note unlike the array classes this isn't picklable.
     """
-    def __init__(self, h5file):
+    def __init__(self, h5file: str) -> None:
 
         self.ordinal, self.categorical, self.coordinates = None, None, None
         self._hfile = tables.open_file(h5file, "r")
@@ -87,10 +90,10 @@ class H5Features:
         if self.ordinal and self.categorical:
             assert len(self.ordinal) == len(self.categorical)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self._n
 
-    def rows(self, rows: FixedSlice):
+    def rows(self, rows: FixedSlice) -> FeatureValues:
         ord_data = None
         cat_data = None
         if self.ordinal:
@@ -99,10 +102,10 @@ class H5Features:
             cat_data = self.categorical[rows.start:rows.stop]
         return FeatureValues(ord_data, cat_data)
 
-    def __del__(self):
+    def __del__(self) -> None:
         self._hfile.close()
 
-def read_image_spec(filename):
+def read_image_spec(filename: str) -> ImageSpec:
     with tables.open_file(filename, mode="r") as h5file:
         x_coordinates = h5file.root.x_coordinates.read()
         y_coordinates = h5file.root.y_coordinates.read()
