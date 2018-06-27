2530d339afe5a8263a31160ae8270a7ae2ad165a
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2018-06-28 09:39:36 +1000

    Add tensorflow to prerequisites

--- setup.py
+++ setup.py
@@ -39,7 +39,8 @@ setup(
         "lru-dict>=1.1.6",
         "tqdm>=4.19.6",
         "scikit-learn>=0.19.1",
-        "aboleth>=0.7.0"
+        "aboleth>=0.7.0",
+        "tensorflow>=1.7"
     ],
 
     extras_require={
