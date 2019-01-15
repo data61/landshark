1955eb581e3dbefff2f364643d1b65d84580bca6
Dave Cole <david.cole@data61.csiro.au>
2019-01-15 11:44:59 +1100

    Reset gdal dependency to pygdal>=2.0.1 for bracewell support

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "numpy>=1.13.3",
         "scipy>=0.19",
         "click>=6.7",
-        "pygdal>=2.2.3.3",
+        "pygdal>=2.0.1",
         "rasterio>=1.0.2",
         "tables>=3.4.2",
         "pyshp>=1.2.12",
